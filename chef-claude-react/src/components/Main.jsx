86492787666930c355fb7e662e320549f5bb601e
sorcerycss <sorcerycss@gmail.com>
2025-11-20 14:02:28 -0800

    Components refactor + scroll fix

--- chef-claude-react/src/components/Main.jsx
+++ chef-claude-react/src/components/Main.jsx
@@ -1,44 +1,58 @@
 import React from "react";
-
 import IngredientsList from "./IngredientsList";
 import ClaudeRecipe from "./ClaudeRecipe";
+import { getRecipeFromChefClaude } from "../ai";
 
 export default function Main() {
-  const [ingredients, setIngredients] = React.useState([]);
-
+  const [ingredients, setIngredients] = React.useState(["chicken", "all the main spices", "corn", "heavy cream", "pasta"]);
   const [recipeShown, setRecipeShown] = React.useState(false);
+  const [recipeText, setRecipeText] = React.useState("");
 
-  function toggleRecipeShown() {
-    setRecipeShown((prevShown) => !prevShown);
+  const recipeSection = React.useRef(null)
+
+  React.useEffect(() => {
+    if (recipeShown !== "" && recipeSection.current !== null) {
+      recipeSection.current.scrollIntoView({behavir: "smooth"})
+    }
+  }, [recipeShown])
+
+  async function handleGetRecipe() {
+    try {
+      const recipe = await getRecipeFromChefClaude(ingredients);
+      console.log("RECIPE:", recipe);
+      setRecipeText(recipe);
+      setRecipeShown(true);
+    } catch (err) {
+      console.error("Error fetching recipe:", err);
+    }
   }
 
   function addIngredient(formData) {
     const newIngredient = formData.get("ingredient");
-    setIngredients((prevIngerients) => [...prevIngerients, newIngredient]);
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
   }
 
   return (
-    <>
-      <main>
-        <form action={addIngredient} className="add-ingredient-form">
-          <input
-            type="text"
-            placeholder="e.g. oregano"
-            name="ingredient"
-            aria-label="Add ingredient"
-          />
-          <button className="btn">Add ingredient</button>
-        </form>
+    <main>
+      <form action={addIngredient} className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
 
-        {ingredients.length > 0 && (
-          <IngredientsList
-            ingredients={ingredients}
-            toggleRecipeShown={toggleRecipeShown}
-          />
-        )}
+      {ingredients.length > 0 && (
+        <IngredientsList
+          ref={recipeSection}
+          ingredients={ingredients}
+          handleGetRecipe={handleGetRecipe}
+        />
+      )}
 
-        {recipeShown && <ClaudeRecipe />}
-      </main>
-    </>
+      {recipeShown && <ClaudeRecipe recipeText={recipeText} />}
+    </main>
   );
 }
